94770ed3cb4e47754a56c37cb7648a4b00257563
Wijnand Warren <god@ansuz.nl>
2012-09-26 21:57:47 +0200

    Updating SkillVO to reflect the data the the Skills API responses are returning.

--- src/nl/ansuz/android/glitch/api/vo/players/SkillVO.java
+++ src/nl/ansuz/android/glitch/api/vo/players/SkillVO.java
@@ -15,4 +15,47 @@ public class SkillVO {
 	@SerializedName("name")
 	public String name;
 	
+	@SerializedName("class_tsid")
+	public String classTsid;
+	
+	@SerializedName("url")
+	public String url;
+	
+	@SerializedName("description")
+	public String description;
+	
+	@SerializedName("icon_44")
+	public String icon44;
+	
+	@SerializedName("icon_100")
+	public String icon100;
+	
+	@SerializedName("icon_460")
+	public String icon460;
+	
+	@SerializedName("total_time")
+	public int totalTime;
+	
+	@SerializedName("time_remaining")
+	public int timeRemaining;
+	
+	@SerializedName("required_level")
+	public String requiredLevel;
+	
+	// List of skill class TSIDs that are required before learning this skill.
+	@SerializedName("required_skills")
+	public String[] requiredSkills;
+	
+	@SerializedName("unlearn_quest_removal")
+	public int unlearnQuestRemoval;
+	
+	@SerializedName("unlearn_time")
+	public int unlearnTime;
+	
+	@SerializedName("time_start")
+	public int timeStart;
+	
+	@SerializedName("time_complete")
+	public int timeComplete;
+	
 }
