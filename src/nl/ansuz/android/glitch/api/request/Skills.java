4686f2386a5bf603829c02b9142f7e282c84df4b
Wijnand Warren <god@ansuz.nl>
2012-09-28 23:27:49 +0200

    Adding more convenience methods to create Skills API requests.

--- src/nl/ansuz/android/glitch/api/request/Skills.java
+++ src/nl/ansuz/android/glitch/api/request/Skills.java
@@ -1,5 +1,7 @@
 package nl.ansuz.android.glitch.api.request;
 
+import java.util.HashMap;
+
 import com.tinyspeck.android.Glitch;
 import com.tinyspeck.android.GlitchRequest;
 
@@ -36,7 +38,8 @@ public class Skills {
 	 * @author Wijnand
 	 */
 	public class Parameter {
-		
+		public static final String SKILL_ID = "skill_id";
+		public static final String SKILL_CLASS = "skill_class";
 	}
 	
 	/**
@@ -46,19 +49,66 @@ public class Skills {
 	 *            Reference to the instance of the Glitch API object.
 	 * @param method
 	 *            The Players method to invoke.
+	 * @param parameters The parameters to send with the API request.
 	 */
-	private static GlitchRequest createGenericSkillsRequest(Glitch glitch, String method) {
-		return new GlitchRequest(method, glitch);
+	private static GlitchRequest createGenericSkillsRequest(Glitch glitch, String method, HashMap<String, String> parameters) {
+		return new GlitchRequest(method, parameters, glitch);
+	}
+	
+	private static GlitchRequest createSingleParameterRequest(Glitch glitch, String method, String parameterName, String parameterValue) {
+		HashMap<String, String> params = new HashMap<String, String>();
+		params.put(parameterName, parameterValue);
+		
+		return createGenericSkillsRequest(glitch, method, params);
+	}
+	
+	/**
+	 * Creates a GlitchRequest object to retrieve information about a skill. Includes state information if authenticated.
+	 * 
+	 * @see http://developer.glitch.com/api/explore/#!skills.getInfo
+	 * @see createGenericSkillsRequest(glitch, method, parameters)
+	 * 
+	 * @param parameterName Use either Parameter.SKILL_ID or Parameter.SKILL_CLASS
+	 * @param parameterValue The value of the parameter to pass.
+	 */
+	public static GlitchRequest createGetInfoRequest(Glitch glitch, String parameterName, String parameterValue) {
+		return createSingleParameterRequest(glitch, Method.GET_INFO, parameterName, parameterValue);
+	}
+	
+	/**
+	 * Creates a GlitchRequest object to start learning a new skill for the authenticated player.
+	 * 
+	 * @see http://developer.glitch.com/api/explore/#!skills.learn
+	 * @see createGenericSkillsRequest(glitch, method, parameters)
+	 * 
+	 * @param parameterName Use either Parameter.SKILL_ID or Parameter.SKILL_CLASS
+	 * @param parameterValue The value of the parameter to pass.
+	 */
+	public static GlitchRequest createLearnRequest(Glitch glitch, String parameterName, String parameterValue) {
+		return createSingleParameterRequest(glitch, Method.LEARN, parameterName, parameterValue);
 	}
 	
 	/**
 	 * Creates a GlitchRequest object to retrieve the authenticated player's currently unlearning skill, if any, and when it is currently scheduled to finish.
 	 * 
 	 * @see http://developer.glitch.com/api/explore/#!skills.listUnlearning
-	 * @see createGenericSkillsRequest(Glitch glitch, String method)
+	 * @see createGenericSkillsRequest(glitch, method, parameters)
 	 */
 	public static GlitchRequest createListUnlearningRequest(Glitch glitch) {
-		return createGenericSkillsRequest(glitch, Method.LIST_UNLEARNING);
+		return createGenericSkillsRequest(glitch, Method.LIST_UNLEARNING, null);
+	}
+	
+	/**
+	 * Creates a GlitchRequest object to start unlearning a skill for the authenticated player.
+	 * 
+	 * @see http://developer.glitch.com/api/explore/#!skills.unlearn
+	 * @see createGenericSkillsRequest(glitch, method, parameters)
+	 * 
+	 * @param parameterName Use either Parameter.SKILL_ID or Parameter.SKILL_CLASS
+	 * @param parameterValue The value of the parameter to pass.
+	 */
+	public static GlitchRequest createUnlearnRequest(Glitch glitch, String parameterName, String parameterValue) {
+		return createSingleParameterRequest(glitch, Method.UNLEARN, parameterName, parameterValue);
 	}
 
 }
